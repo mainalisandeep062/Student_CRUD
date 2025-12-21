965b35c67d1d5b545cbbab54180c6e43d5b3cafe
Sandeep <sawondeep4@gmail.com>
2025-12-22 00:50:41 +0545

    Initial commit

---
+++ src/main/java/com/texas/springboot/student_rest/Models/Student.java
@@ -0,0 +1,23 @@
+package com.texas.springboot.student_rest.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Integer age;
+}
