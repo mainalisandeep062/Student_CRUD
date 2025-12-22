d3005e2cbeb9aeac1c1b7552bb261ffc5025f6ad
Sandeep <sawondeep4@gmail.com>
2025-12-22 22:36:27 +0545

    all models ready

--- src/main/java/com/texas/springboot/student_rest/Models/Course.java
+++ src/main/java/com/texas/springboot/student_rest/Models/Course.java
@@ -1,4 +1,30 @@
 package com.texas.springboot.student_rest.Models;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Course", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "course_id", name = "uk_course_id")
+})
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Column(unique = true)
+    private String code;
+
+    private String name;
+
+    private int credits;
+
+    @ManyToOne
+    private Department department;
 }
--- src/main/java/com/texas/springboot/student_rest/Models/Enrollment.java
+++ src/main/java/com/texas/springboot/student_rest/Models/Enrollment.java
@@ -1,4 +1,30 @@
 package com.texas.springboot.student_rest.Models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Enrollment", uniqueConstraints ={
+        @UniqueConstraint(columnNames = "enroll_id", name = "uk_enroll_id")
+})
 public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "enroll_id")
+    private Long id;
+
+    @ManyToOne
+    private Student student;
+
+    @ManyToOne
+    private Course course;
+
+
+    private String semester;
+
+    private String grade;
 }
